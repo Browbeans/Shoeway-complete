d20fc6f07ad4796d4b0a2b1e79ee0b3218258c56
Oliver Johansson <oliverjohansson3@gmail.com>
2021-05-18 13:30:16 +0200

    Test

--- Server/Resources/Order/order.router.ts
+++ Server/Resources/Order/order.router.ts
@@ -8,6 +8,5 @@ const OrderRouter = express.Router()
 OrderRouter.post('/add-order', controller.addOrder)
 OrderRouter.get('/all-orders', controller.getOrders)
 OrderRouter.get('/specific-order/:id', controller.getSpecific)
-OrderRouter.get('/order-test', async(req: Request, res: Response) => res.send('order-test'))
 
 export default OrderRouter
