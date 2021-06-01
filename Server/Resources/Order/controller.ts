88d0210324722a66ca9967681f0c9f3064297bc3
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-06-01 12:16:44 +0200

    handling error order done

--- Server/Resources/Order/controller.ts
+++ Server/Resources/Order/controller.ts
@@ -1,16 +1,25 @@
 const Orders = require('./order.model')
 const Users = require('../Users/users-model')
 const Products = require('../Products/product.model')
-import { Request, Response } from 'express'
+const ApiError = require("../../Error/ApiError");
+import { Request, Response, NextFunction } from 'express'
 
 
 
-module.exports.addOrder = async (req: Request, res: Response) => {
+module.exports.addOrder = async (req: Request, res: Response, next: NextFunction) => {
     const { ordernumber, products, customer, orderAmount } = req.body
     const productVariant: any = []
 
+    let isError = false;
+
     products.map(async (productID: any) =>  {
+
         const singleProduct = await Products.findOne({_id: productID.id})
+
+        if(!singleProduct){
+            isError = true;
+        } else {
+
         singleProduct.variants.forEach(async (product: {_id: string, size: string, stock: number, quantity: number, title: string}) => {
             if(productID.size === product.size) {
                 const variantArray = [ {
@@ -38,6 +47,7 @@ module.exports.addOrder = async (req: Request, res: Response) => {
                 )
             }
         })
+    }
     })
 
 
@@ -49,49 +59,71 @@ module.exports.addOrder = async (req: Request, res: Response) => {
             orderAmount: orderAmount,
             isSent: false
         })
-    
-        newOrder.save()
-        res.status(200).json('Order Made')
-        console.log('efter loopen') 
+        if(!isError){
+            newOrder.save()
+            res.status(200).json('Order Made')
+        } else {
+             next(ApiError.badRequest("Couldnt add the order"));
+             return;
+        }
     }, 50);
     
-    // const idArray: any = []
-    // products.forEach((element: any) => {
-    //     idArray.push(element.id)
-    // });
+}
 
-    // const currentProduct = await Products.find({ '_id': { $in: idArray } });
-    // console.log(currentProduct)
+module.exports.getOrders = async function (req: Request, res: Response, next: NextFunction) {
+    const result = await Orders.find()
+
+    if(result){
+        res.status(200).json(result);
+    } else {
+        next(ApiError.badRequest('Something went wrong'));
+        return;
+    }
     
 }
 
-module.exports.getOrders = async function (req: Request, res: Response) {
-    const result = await Orders.find()
-    res.status(200).json(result)
-}
-
-module.exports.getSpecific = async function(req: Request, res: Response) {
+module.exports.getSpecific = async function(req: Request, res: Response, next: NextFunction) {
     const id = req.params.id
     const order = await Orders.findById(id)
-    res.status(200).json(order)
+
+    if(order){
+      res.status(200).json(order);
+    } else {
+       next(ApiError.notFound('Couldnt find the specific order'));
+       return;
+    }
 }
 
-module.exports.getUserOrders = async function(req: Request, res: Response) {
+module.exports.getUserOrders = async function(req: Request, res: Response, next: NextFunction) {
     const urlId = req.params.id
     const user = await Users.findById(urlId)
     const userOrder = await Orders.find({customer: user._id}).populate('customer')
 
-    res.status(200).json(userOrder)
+    if(user){
+        res.status(200).json(user);
+
+        if (userOrder) {
+         return res.status(200).json(userOrder);
+
+        } else {
+          next(ApiError.notFound("Couldnt find the the order"));
+          return;
+
+        }
+    } else {
+        next(ApiError.notFound("Couldnt find the the user"));
+        return;
+    }
 }
 
-module.exports.getOrderProducts = async function(req: Request, res: Response) {
-    const orderId = req.params.id
-    const orderProducts = await Orders.findById(orderId).populate('products')
-    res.status(200).json(orderProducts)
-}
-
-module.exports.orderSent = async function(req: Request, res: Response) {
+module.exports.orderSent = async function(req: Request, res: Response, next: NextFunction) {
     const orderId = req.params.id
     const order = await Orders.updateOne({ _id: orderId}, { isSent: true})
-    res.status(200).json(order)
+
+    if(order){
+        return res.status(200).json(order);
+    } else {
+        next(ApiError.badRequest('Couldnt update the order'));
+        return;
+    }
 }
